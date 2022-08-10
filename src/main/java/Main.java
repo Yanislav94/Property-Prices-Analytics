424a7d4790aefaad5977cbef4c88ca73d907a987
Yanislav <ytomo@softserveinc.com>
2022-08-10 20:50:35 +0300

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,39 @@
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.*;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        //URL url = new URL("https://www.mirela.bg/index.php?p=stats_list&price_type=1&type=1&etype=543&city_id=3&week=2022-08-08&month=2022-07-01");
+        String url = "https://www.mirela.bg/index.php?p=stats_list&price_type=1&type=1&etype=543&city_id=3&week=2022-08-08&month=2022-07-01";
+
+        Document doc = Jsoup.connect(url).get();
+        Elements desktopOnly = doc.getElementsByClass("style4");
+        System.out.println(desktopOnly);
+
+    }
+
+    static void httpRequest(URL url, HttpURLConnection con) throws IOException {
+        con.setRequestMethod("POST");
+        int status = con.getResponseCode();
+    }
+
+    static String readResponse(HttpURLConnection con) throws IOException {
+
+        BufferedReader in = new BufferedReader(
+                new InputStreamReader(con.getInputStream()));
+        String inputLine;
+        StringBuilder content = new StringBuilder();
+        while ((inputLine = in.readLine()) != null) {
+            content.append(inputLine);
+        }
+        in.close();
+        return content.toString();
+    }
+}
